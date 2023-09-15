969b0b6479585824bbef955ef04e1447a2d226e4
arad <aradarvinxo@gmail.com>
2023-09-15 15:24:21 +0330

    Chore: custom user model

--- stationary_store/accounts/models.py
+++ stationary_store/accounts/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.base_user import AbstractBaseUser
+from .utils import phone_validator
+from .managers import CustomUserManager
 # Create your models here.
+
+
+class CustomUser(AbstractBaseUser):
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(validators=phone_validator)
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'email']
+
+    objects = CustomUserManager()
+
+    def __str__(self) -> str:
+        return f"{self.username}"
