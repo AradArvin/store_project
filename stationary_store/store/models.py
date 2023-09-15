04ca56a5ab5bdd7fde9557d2b4b7b0d9972234d5
arad <aradarvinxo@gmail.com>
2023-09-15 16:58:32 +0330

    Chore: order model

--- stationary_store/store/models.py
+++ stationary_store/store/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Order(models.Model):
+
+    class Status(models.TextChoices):
+        PENDING = "P", _("Pending")
+        BAN = "B", _("Banned")
+        CONFIRM = "C", _("Confirmed")
+
+
+    customer_id = models.ForeignKey(CustomUser, on_delete=models.SET_NULL, null=True)
+    order_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(choices=Status.choices, default="P")
+
+
