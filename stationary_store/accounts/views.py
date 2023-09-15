bf83841eb70fcb88d62cf2e794abbc6d3afb79bc
arad <aradarvinxo@gmail.com>
2023-09-15 16:29:41 +0330

    Chore: some imports

--- stationary_store/accounts/views.py
+++ stationary_store/accounts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import CustomUser
+from .serializers import AdressSerializer, UserSignUpSerializer, UserLoginSerializer, OtpSerializer
 # Create your views here.
+
+
