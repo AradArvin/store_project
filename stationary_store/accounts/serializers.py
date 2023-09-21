64999aaa7738d98085327a75cc7e3e7c963baabd
arad <aradarvinxo@gmail.com>
2023-09-21 09:08:50 +0330

    Chore: user login serializer done

--- stationary_store/accounts/serializers.py
+++ stationary_store/accounts/serializers.py
@@ -19,6 +19,49 @@ class SignUpSerializer(serializers.ModelSerializer):
         return CustomUser.objects.create_user(**validated_data)
 
 
+
+class UserLoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=128, write_only=True)
+    access_token = serializers.CharField(max_length=255, read_only=True)
+    refresh_token = serializers.CharField(max_length=255, read_only=True)
+
+
+    def validate(self, attrs):
+        
+        email = attrs.get('email', None)
+        password = attrs.get('password', None)
+
+        if email is None:
+            raise serializers.ValidationError("An email adress is required to login")
+       
+        if password is None:
+            raise serializers.ValidationError("A password is required to login")
+        
+        user = authenticate(username=email, password=password)
+        
+
+        if user is None:
+            raise serializers.ValidationError("User not found!")
+        
+
+        if not user.is_active:
+            raise serializers.ValidationError("This user has been deavtivated")
+        
+        access_token = access_token_gen(user.pk)
+        refresh_token = refresh_token_gen(user.pk)
+
+
+        validated_data = {
+            'email': user.email,
+            'username': user.username,
+            'access_token': access_token,
+            'refresh_token': refresh_token
+        }
+    
+        return validated_data
+    
+
     class Meta:
         model = CustomUser
         fields = ['username', 'password']
