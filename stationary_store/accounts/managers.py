e46f0ee7d479b6b607410e466a0ca7c1171dbd2d
arad <aradarvinxo@gmail.com>
2023-09-21 09:04:36 +0330

    Refactor: custom user manager changed to fit jwt

--- stationary_store/accounts/managers.py
+++ stationary_store/accounts/managers.py
@@ -1,29 +1,45 @@
+from typing import Any
 from django.contrib.auth.models import BaseUserManager
+from rest_framework.exceptions import APIException
+
+
+
+class UnmatchedException(APIException):
+    status_code = 401
+    default_detail = 'Not Acceptable'
+    default_code = '403'
 
 
 
 class CustomUserManager(BaseUserManager):
-    '''
-    Base user manager to create user and superuser
-    '''
-    def create_user(self, username, email, password, **other_fields):
-        other_fields.setdefault('is_active', True)
 
-        if not email:
-            raise ValueError('Please provide an Email adress')
-
-        user = self.model(email=email, username=username, **other_fields)
+    def create_user(self, username, email, password=None, **kwargs):
+        
+        if username is None:
+            raise TypeError("User must have a username!")
+        
+        if email is None:
+            raise TypeError("User must have an email!")
+        
+        if password != kwargs["confirm_password"]:
+            raise UnmatchedException("Passwords doesn't match")
+        
+       
+        user = self.model(username=username, email=email)
         user.set_password(password)
         user.save()
-
+        
         return user
+    
 
-    def create_superuser(self, username, email, password, **other_fields):
+    def create_superuser(self, username, email, password, **kwargs):
+        if password is None:
+            raise TypeError("User must have a password!")
         
-        other_fields.setdefault('is_staff', True)
-        other_fields.setdefault('is_superuser', True)
-        other_fields.setdefault('is_active', True)
 
-        user = self.create_user(self, username, email, password, **other_fields)
-        
+        user = self.create_user(username=username, email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+
         return user
