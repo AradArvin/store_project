f8c6bd5c5f1437db8115e4814b0b971e629eb06d
arad <aradarvinxo@gmail.com>
2023-09-21 14:53:54 +0330

    Chore: url for adress list apiview

--- stationary_store/accounts/urls.py
+++ stationary_store/accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('api/signup', SignUpAPIView.as_view(), name='signup'),
     path('api/login', LoginAPIView.as_view(), name='login'),
     path('api/user', UserProfileAPIView.as_view(), name='user'),
+    path('api/adress', AdressListView.as_view(), name='adress'),
 ]
