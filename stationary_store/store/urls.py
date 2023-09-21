30d8c3132f25f1e79b69007db9844f7c766ef69b
arad <aradarvinxo@gmail.com>
2023-09-21 15:50:12 +0330

    Chore: urls added for employee

--- stationary_store/store/urls.py
+++ stationary_store/store/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import *
-
+from employee.views import EmployeeViewSet
 
 
 router = DefaultRouter()
@@ -9,7 +9,7 @@ router.register(r'categories', CategoryViewSet, basename='category')
 router.register(r'items', ItemsViewSet, basename='item')
 router.register(r'orders', OrderViewSet, basename='order')
 router.register(r'orderitems', OrderItemViewSet, basename='orderitem')
-
+router.register(r'employee', EmployeeViewSet, basename='employee')
 
 
 urlpatterns = [
