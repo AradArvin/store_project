48abac938e2488a7699534e50d9b208406a716ab
arad <aradarvinxo@gmail.com>
2023-09-21 09:46:51 +0330

    Chore: employee viewset

--- stationary_store/employee/views.py
+++ stationary_store/employee/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import viewsets
 
+from .models import Employee
+from .serializers import EmployeeSerializer
 # Create your views here.
+
+
+
+class EmployeeViewSet(viewsets.ModelViewSet):
+    queryset = Employee.objects.all()
+    serializer_class = EmployeeSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
