af0e89b8c5f04b72122315690ab1d8373b95b1bf
arad <aradarvinxo@gmail.com>
2023-09-21 11:59:44 +0330

    Chore: View sets for models

--- stationary_store/store/views.py
+++ stationary_store/store/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .permissions import IsOwnerOrReadOnly
+from .models import Category, Items, Order, OrderItem
+from .serializers import *
 # Create your views here.
+
+
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+
+
+class ItemsViewSet(ModelViewSet):
+    queryset = Items.objects.all()
+    serializer_class = ItemsSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+
+
+class OrderViewSet(ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
+
+
+class OrderItemViewSet(ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
