091d62c10833248a88d37653c14aac0df7b2efbd
arad <aradarvinxo@gmail.com>
2023-09-21 11:24:16 +0330

    Chore: first version serializers for models

---
+++ stationary_store/store/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from .models import Category, Items, Order, OrderItem
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+        
+
+
+class ItemsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Items
+        fields = '__all__'
+        depth = 2
+
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = '__all__'
+        depth = 2
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
