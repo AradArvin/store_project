38ec45f71f671f5106b5aaa73c867d332a6b8baf
arad <aradarvinxo@gmail.com>
2023-09-21 09:10:06 +0330

    Chore: access token gen func

--- stationary_store/accounts/utils.py
+++ stationary_store/accounts/utils.py
@@ -1,5 +1,24 @@
+import jwt
+from uuid import uuid4
 from django.core.validators import RegexValidator
+from datetime import datetime, timedelta
+from django.conf import settings
 
 
 _REGEX = r'^(\+989|09)+\d{9}$'
 phone_validator = RegexValidator(regex=_REGEX, message="Invalid Phone Number!")
+
+
+def access_token_gen(user_id: int):
+    """Generate access token based on usser id."""
+
+    access_token = token_encode({
+        'token_type':'access',
+        'user_id':user_id,
+        'exp': datetime.utcnow() + timedelta(minutes=1),
+        'iat': datetime.utcnow(),
+        'jti':gen_jti()
+    })
+
+    return access_token
+
