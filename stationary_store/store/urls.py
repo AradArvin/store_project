b1e4deaf7c6d5103d7bffdf9e8edc861616d14ec
arad <aradarvinxo@gmail.com>
2023-09-21 11:59:24 +0330

    Chore: url routes for view sets

--- stationary_store/store/urls.py
+++ stationary_store/store/urls.py
@@ -1,6 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import *
+
+
+
+router = DefaultRouter()
+router.register(r'categories', CategoryViewSet, basename='category')
+router.register(r'items', ItemsViewSet, basename='item')
+router.register(r'orders', OrderViewSet, basename='order')
+router.register(r'orderitems', OrderItemViewSet, basename='orderitem')
+
 
 
 urlpatterns = [
-    path(""),
+    path('', include(router.urls)),
 ]
