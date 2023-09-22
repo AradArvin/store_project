c34a9cd07cab8272d19cc6d8baa75395ce8e6519
arad <aradarvinxo@gmail.com>
2023-09-22 15:57:05 +0330

    Chore: cart movel view set

--- stationary_store/store/views.py
+++ stationary_store/store/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import status
+from rest_framework.response import Response
+
 from .permissions import IsOwnerOrReadOnly
 from .models import Category, Items, Order, OrderItem
 from .serializers import *
+from .renderers import CartJSONRenderer
+
 # Create your views here.
 
 
@@ -12,7 +17,7 @@ from .serializers import *
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 
@@ -20,7 +25,7 @@ class CategoryViewSet(ModelViewSet):
 class ItemsViewSet(ModelViewSet):
     queryset = Items.objects.all()
     serializer_class = ItemsSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 
@@ -28,7 +33,7 @@ class ItemsViewSet(ModelViewSet):
 class OrderViewSet(ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 
@@ -36,4 +41,19 @@ class OrderViewSet(ModelViewSet):
 class OrderItemViewSet(ModelViewSet):
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
+
+class CartViewSet(ModelViewSet):
+    queryset = Cart.objects.all()
+    permission_classes = [IsAuthenticatedOrReadOnly]
+    serializer_class = CartSerializer
+
+    # def post(self, request):
+    #     cart = request.data.get('cart', {})
+        
+    #     serializer = self.serializer_class(data=cart)
+    #     serializer.is_valid(raise_exception=True)
+        
+    #     return Response(serializer.data, status=status.HTTP_200_OK)
