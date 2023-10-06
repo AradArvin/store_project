f397151c225d9b02732550165a6866b453f1e100
arad <aradarvinxo@gmail.com>
2023-10-06 10:14:15 +0330

    Chore: view for shoping cart

--- stationary_store/store/views.py
+++ stationary_store/store/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny, IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework import filters
+from rest_framework.pagination import PageNumberPagination
+from django.shortcuts import get_object_or_404
 
 from .permissions import IsOwnerOrReadOnly
 from .models import Category, Items, Order, OrderItem
@@ -12,47 +15,108 @@ from .serializers import *
 # Create your views here.
 
 
+class CustomPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    max_page_size = 1000
 
-class CategoryViewSet(ModelViewSet):
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "count": self.page.paginator.count,
+                "total_pages": self.page.paginator.num_pages,
+                "results": data,
+            }
+        )
+
+
+
+class CategoryAPIView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticatedOrReadOnly,]
+    pagination_class = CustomPagination
 
 
 
-
-class ItemsViewSet(ModelViewSet):
+class ItemsAPIView(generics.ListAPIView):
     queryset = Items.objects.all()
     serializer_class = ItemsSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny,]
+    filter_backends = [filters.SearchFilter,]
+    search_fields = ["name", "description"]
+    pagination_class = CustomPagination
 
 
+class ShoppingCartAPIView(APIView):
+
+    def get_cart(self, request):
+        cart = request.session.get("cart", [])
+
+        cart_details = []
+        total_price = 0
+
+        for item in cart:
+            product = get_object_or_404(Items, pk=item["item_id"])
+            quantity = item["quantity"]
+            item_total = product.price * quantity
+            total_price += item_total
+
+            cart_details.append(
+                {
+                    "item": ItemsSerializer(product).data,
+                    "quantity": quantity,
+                    "item_total": item_total,
+                }
+            )
+
+        return cart_details, total_price
 
 
-class OrderViewSet(ModelViewSet):
-    queryset = Order.objects.all()
-    serializer_class = OrderSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    def get(self, request):
+        cart_details, total_price = self.get_cart(request)
+        return Response({"cart_items": cart_details, "total_price": total_price})
 
 
+    def post(self, request):
+        item_id = request.data.get("item_id")
+        quantity = int(request.data.get("quantity", 1))
+
+        product = get_object_or_404(Items, pk=item_id)
+
+        cart = request.session.get("cart", [])
+
+        for item in cart:
+            if item["item_id"] == item_id:
+                item["quantity"] += quantity
+                request.session.modified = True
+                cart_details, total_price = self.get_cart(request)
+                return Response(
+                    {"cart_items": cart_details, "total_price": total_price}
+                )
+
+        cart.append({"item_id": item_id, "quantity": quantity})
+        request.session["cart"] = cart
+        request.session.modified = True
+
+        cart_details, total_price = self.get_cart(request)
+        return Response({"cart_items": cart_details, "total_price": total_price})
 
 
-class OrderItemViewSet(ModelViewSet):
-    queryset = OrderItem.objects.all()
-    serializer_class = OrderItemSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    def delete(self, request):
+        item_id = request.data.get("item_id")
 
+        cart = request.session.get("cart", [])
 
+        for item in cart:
+            if item["item_id"] == item_id:
+                cart.remove(item)
+                request.session.modified = True
+                cart_details, total_price = self.get_cart(request)
+                return Response(
+                    {"cart_items": cart_details, "total_price": total_price}
+                )
 
-class CartViewSet(ModelViewSet):
-    queryset = Cart.objects.all()
-    permission_classes = [IsAuthenticatedOrReadOnly]
-    serializer_class = CartSerializer
-
-    # def post(self, request):
-    #     cart = request.data.get('cart', {})
-        
-    #     serializer = self.serializer_class(data=cart)
-    #     serializer.is_valid(raise_exception=True)
-        
-    #     return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_404_NOT_FOUND)
