b060c1a5f418e85a300c818a4ee74686666f09da
arad <aradarvinxo@gmail.com>
2023-10-06 10:11:21 +0330

    Chore: url for logout

--- stationary_store/accounts/urls.py
+++ stationary_store/accounts/urls.py
@@ -6,6 +6,7 @@ from .views import *
 urlpatterns = [
     path('api/signup', SignUpAPIView.as_view(), name='signup'),
     path('api/login', LoginAPIView.as_view(), name='login'),
+    path('api/logout', LogOutAPIView.as_view(), name='logout'),
     path('api/user', UserProfileAPIView.as_view(), name='user'),
     path('api/adress', AdressListView.as_view(), name='adress'),
 ]
